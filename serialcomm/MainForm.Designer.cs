4ad6c90c3455d84311ac62cb267022625e2edf0f
Justinas <a>
2014-12-08 14:55:27 +0200

    initial

---
+++ serialcomm/MainForm.Designer.cs
@@ -0,0 +1,300 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Justinas
+ * Date: 2011.08.02
+ * Time: 19:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace serialcomm
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.baudCmb = new System.Windows.Forms.ComboBox();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.portCmb = new System.Windows.Forms.ComboBox();
+            this.logRtxt = new System.Windows.Forms.RichTextBox();
+            this.inputTxt = new System.Windows.Forms.TextBox();
+            this.eolCmb = new System.Windows.Forms.ComboBox();
+            this.inputModeCmb = new System.Windows.Forms.ComboBox();
+            this.autoConnectChk = new System.Windows.Forms.CheckBox();
+            this.clearLogBtn = new System.Windows.Forms.Button();
+            this.lineBreakChk = new System.Windows.Forms.CheckBox();
+            this.logLimitCmb = new System.Windows.Forms.ComboBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // baudCmb
+            // 
+            this.baudCmb.BackColor = System.Drawing.Color.Black;
+            this.baudCmb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.baudCmb.ForeColor = System.Drawing.Color.White;
+            this.baudCmb.FormattingEnabled = true;
+            this.baudCmb.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.baudCmb.Location = new System.Drawing.Point(287, 14);
+            this.baudCmb.Name = "baudCmb";
+            this.baudCmb.Size = new System.Drawing.Size(71, 21);
+            this.baudCmb.TabIndex = 0;
+            this.baudCmb.Text = "115200";
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.BackColor = System.Drawing.Color.Black;
+            this.connectBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectBtn.ForeColor = System.Drawing.Color.White;
+            this.connectBtn.Location = new System.Drawing.Point(12, 12);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(95, 23);
+            this.connectBtn.TabIndex = 1;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = false;
+            this.connectBtn.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(224, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Baud rate:";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(113, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port:";
+            // 
+            // portCmb
+            // 
+            this.portCmb.BackColor = System.Drawing.Color.Black;
+            this.portCmb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.portCmb.ForeColor = System.Drawing.Color.White;
+            this.portCmb.FormattingEnabled = true;
+            this.portCmb.Location = new System.Drawing.Point(152, 14);
+            this.portCmb.Name = "portCmb";
+            this.portCmb.Size = new System.Drawing.Size(66, 21);
+            this.portCmb.TabIndex = 4;
+            this.portCmb.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ComboBox2MouseClick);
+            // 
+            // logRtxt
+            // 
+            this.logRtxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logRtxt.BackColor = System.Drawing.Color.Black;
+            this.logRtxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logRtxt.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.logRtxt.ForeColor = System.Drawing.Color.White;
+            this.logRtxt.Location = new System.Drawing.Point(12, 41);
+            this.logRtxt.Name = "logRtxt";
+            this.logRtxt.Size = new System.Drawing.Size(923, 316);
+            this.logRtxt.TabIndex = 5;
+            this.logRtxt.Text = "";
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTxt.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.inputTxt.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
+            this.inputTxt.BackColor = System.Drawing.Color.Black;
+            this.inputTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.inputTxt.ForeColor = System.Drawing.Color.White;
+            this.inputTxt.Location = new System.Drawing.Point(12, 363);
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.Size = new System.Drawing.Size(762, 20);
+            this.inputTxt.TabIndex = 6;
+            this.inputTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1KeyDown);
+            // 
+            // eolCmb
+            // 
+            this.eolCmb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.eolCmb.BackColor = System.Drawing.Color.Black;
+            this.eolCmb.Enabled = false;
+            this.eolCmb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.eolCmb.ForeColor = System.Drawing.Color.White;
+            this.eolCmb.FormattingEnabled = true;
+            this.eolCmb.Items.AddRange(new object[] {
+            "None",
+            "LF",
+            "CR",
+            "CR LF"});
+            this.eolCmb.Location = new System.Drawing.Point(873, 363);
+            this.eolCmb.Name = "eolCmb";
+            this.eolCmb.Size = new System.Drawing.Size(62, 21);
+            this.eolCmb.TabIndex = 8;
+            this.eolCmb.Text = "LF";
+            this.toolTip1.SetToolTip(this.eolCmb, "End of line symbol for \"Line mode\"");
+            // 
+            // inputModeCmb
+            // 
+            this.inputModeCmb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputModeCmb.BackColor = System.Drawing.Color.Black;
+            this.inputModeCmb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inputModeCmb.ForeColor = System.Drawing.Color.White;
+            this.inputModeCmb.FormattingEnabled = true;
+            this.inputModeCmb.Items.AddRange(new object[] {
+            "Line mode",
+            "Text mode",
+            "Hex mode",
+            "Mixed mode"});
+            this.inputModeCmb.Location = new System.Drawing.Point(780, 363);
+            this.inputModeCmb.Name = "inputModeCmb";
+            this.inputModeCmb.Size = new System.Drawing.Size(87, 21);
+            this.inputModeCmb.TabIndex = 9;
+            this.inputModeCmb.Text = "Hex mode";
+            this.toolTip1.SetToolTip(this.inputModeCmb, "Hex mode - for ex. \"40 41 1A\"\r\nLine mode - for ex. \"Line text\" (automatically sen" +
+        "ds end of line byte(s))\r\nMixed mode - for ex. \"Line text<0d 0a>\" (sends text and" +
+        " Hex data enclosed in <>)");
+            this.inputModeCmb.SelectedIndexChanged += new System.EventHandler(this.ComboBox4SelectedIndexChanged);
+            // 
+            // autoConnectChk
+            // 
+            this.autoConnectChk.AutoSize = true;
+            this.autoConnectChk.ForeColor = System.Drawing.SystemColors.Control;
+            this.autoConnectChk.Location = new System.Drawing.Point(364, 16);
+            this.autoConnectChk.Name = "autoConnectChk";
+            this.autoConnectChk.Size = new System.Drawing.Size(90, 17);
+            this.autoConnectChk.TabIndex = 10;
+            this.autoConnectChk.Text = "Auto connect";
+            this.toolTip1.SetToolTip(this.autoConnectChk, "Auto connect when serialcomm window is activated and auto disconnect when deactiv" +
+        "ated");
+            this.autoConnectChk.UseVisualStyleBackColor = true;
+            // 
+            // clearLogBtn
+            // 
+            this.clearLogBtn.BackColor = System.Drawing.Color.Black;
+            this.clearLogBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearLogBtn.ForeColor = System.Drawing.Color.White;
+            this.clearLogBtn.Location = new System.Drawing.Point(544, 12);
+            this.clearLogBtn.Name = "clearLogBtn";
+            this.clearLogBtn.Size = new System.Drawing.Size(95, 23);
+            this.clearLogBtn.TabIndex = 11;
+            this.clearLogBtn.Text = "Clear log";
+            this.clearLogBtn.UseVisualStyleBackColor = false;
+            this.clearLogBtn.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // lineBreakChk
+            // 
+            this.lineBreakChk.AutoSize = true;
+            this.lineBreakChk.ForeColor = System.Drawing.SystemColors.Control;
+            this.lineBreakChk.Location = new System.Drawing.Point(458, 16);
+            this.lineBreakChk.Name = "lineBreakChk";
+            this.lineBreakChk.Size = new System.Drawing.Size(76, 17);
+            this.lineBreakChk.TabIndex = 12;
+            this.lineBreakChk.Text = "Line break";
+            this.toolTip1.SetToolTip(this.lineBreakChk, "Parse line breaks. \r\nIf enabled waits for full line and then prints it.\r\nIf disab" +
+        "led data is printed as it\'s received including line break bytes.");
+            this.lineBreakChk.UseVisualStyleBackColor = true;
+            // 
+            // logLimitCmb
+            // 
+            this.logLimitCmb.BackColor = System.Drawing.Color.Black;
+            this.logLimitCmb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logLimitCmb.ForeColor = System.Drawing.Color.White;
+            this.logLimitCmb.FormattingEnabled = true;
+            this.logLimitCmb.Location = new System.Drawing.Point(702, 14);
+            this.logLimitCmb.Name = "logLimitCmb";
+            this.logLimitCmb.Size = new System.Drawing.Size(121, 21);
+            this.logLimitCmb.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(645, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 18);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Log limit:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(941, 391);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.logLimitCmb);
+            this.Controls.Add(this.lineBreakChk);
+            this.Controls.Add(this.clearLogBtn);
+            this.Controls.Add(this.autoConnectChk);
+            this.Controls.Add(this.inputModeCmb);
+            this.Controls.Add(this.eolCmb);
+            this.Controls.Add(this.inputTxt);
+            this.Controls.Add(this.logRtxt);
+            this.Controls.Add(this.portCmb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.baudCmb);
+            this.MinimumSize = new System.Drawing.Size(456, 143);
+            this.Name = "MainForm";
+            this.Text = "serialcomm";
+            this.Activated += new System.EventHandler(this.MainFormActivated);
+            this.Deactivate += new System.EventHandler(this.MainFormDeactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+		private System.Windows.Forms.ComboBox inputModeCmb;
+		private System.Windows.Forms.ComboBox eolCmb;
+		private System.Windows.Forms.TextBox inputTxt;
+		private System.Windows.Forms.RichTextBox logRtxt;
+		private System.Windows.Forms.ComboBox portCmb;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button connectBtn;
+		private System.Windows.Forms.ComboBox baudCmb;
+        private System.Windows.Forms.CheckBox autoConnectChk;
+        private System.Windows.Forms.Button clearLogBtn;
+        private System.Windows.Forms.CheckBox lineBreakChk;
+        private System.Windows.Forms.ComboBox logLimitCmb;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label3;
+	}
+}
---
+++ serialcomm/MainForm.cs
@@ -0,0 +1,393 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Justinas
+ * Date: 2011.08.02
+ * Time: 19:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using Microsoft.Win32;
+namespace serialcomm
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		AutoCompleteStringCollection	usedCmds = new AutoCompleteStringCollection();
+		int	lastCmd = -1;
+		SerialPort	ser = new SerialPort();
+		Thread		rcvThread = null;
+	    private List<byte> lineBuffer = new List<byte>();
+		
+		byte[]	Hex2Bytes(string hex)
+		{
+			try {
+				string[]	bytesHex = hex.Split(' ', ',', ';');
+				List<byte>	r = new List<byte>();
+				foreach(string b in bytesHex)
+					r.Add(Convert.ToByte(b, 16));
+				return r.ToArray();
+			} catch { return null; }
+		}
+		
+		byte[]	EncodeInputMessage(string inpStr)
+		{
+			char	mode = inputModeCmb.Text[0];
+			if(mode == 'L' || mode == 'T')
+				return System.Text.Encoding.ASCII.GetBytes(inpStr + (mode == 'L' ? GetEOL() : ""));
+			else if(mode == 'H')
+				return Hex2Bytes(inpStr);
+			else
+			{
+				List<byte>	rez = new List<byte>();
+							
+				for(int i = 0, ito = inpStr.Length; i < ito; i++)
+				{
+					if(inpStr[i] == '<')
+					{
+						int end = inpStr.IndexOf('>', i + 1);
+						if(end == -1)
+							return null;
+						
+						string	hexStr = inpStr.Substring(i + 1, end - i - 1);
+						if(hexStr.Length == 0)
+							return null;
+						
+						rez.AddRange(Hex2Bytes(hexStr));
+						
+						i = end;
+					}
+					else
+						rez.Add((byte)inpStr[i]);
+				}
+				
+				return rez.ToArray();
+			}
+		}
+		
+		string	DecodeMessage(byte[] msg)
+		{
+			string	s = "";
+			foreach(byte b in msg)
+			{
+                if(lineBreakChk.Checked && (b == 0x0a || b == 0x0d))
+                    s += (char) b;
+                else if((b < 32 || b >= 128)
+                    && !(b == '\t' && lineBreakChk.Checked))
+					s += "<" + Convert.ToString(b, 16).ToUpper().PadLeft(2, '0') + ">";
+				else
+					s += (char)b;
+			}
+			
+			return s;
+		}
+		
+		void	ReceiveThread()
+		{
+			try {
+				ser.ReadTimeout = 1000;
+				while(ser.IsOpen)
+				{
+					byte[]	buffer = new byte[ser.BytesToRead];
+					int bytes = 0;
+					try { bytes = ser.Read(buffer, 0, buffer.Length); } catch { }
+					
+					if(bytes > 0)
+						Invoke(new D_OnDataReceive(OnDataReceive), buffer, bytes);
+				}
+			} catch(ThreadAbortException) {
+				
+			}
+		}
+		
+		delegate void	D_OnDataReceive(byte[] data, int len);
+		
+		void	LogColored(string text, Color fg, Color bg)
+		{
+			int end = logRtxt.Text.Length;
+			
+			logRtxt.AppendText(text);
+			logRtxt.SelectionStart = end;
+			logRtxt.SelectionLength = text.Length;
+			if(fg != null)
+				logRtxt.SelectionColor = fg;
+			if(bg != null)
+				logRtxt.SelectionBackColor = bg;
+		}
+		
+		void	OnDataReceive(byte[] data, int len)
+		{
+		    data = data.Take(len).ToArray();
+            if(lineBreakChk.Checked)
+            {
+                lineBuffer.AddRange(data);
+                int i;
+                while((i = lineBuffer.IndexOf(0x0a)) >= 0)
+                {
+                    LogMsg(lineBuffer.Take(i).ToArray(), true);
+                    //lineBuffer.Clear();
+                    //lineBuffer.AddRange(data.Skip(i+1));
+                    lineBuffer.RemoveRange(0, i+1);
+                }
+            } else
+            {
+                LogMsg(data, true);
+            }
+		}
+		
+		void	CloseCom()
+		{
+			if(rcvThread != null)
+			{
+				rcvThread.Abort();
+				rcvThread = null;
+			}
+
+            if (ser != null)
+            {
+                if (ser.IsOpen)
+                    ser.Close();
+                ser = null;
+            }
+		}
+		
+		void	LogMsg(byte[] data, bool rcv)
+		{
+            LimitLog();
+
+			LogColored(String.Format("{0:HH:mm:ss:fff}: ", DateTime.Now), Color.Gray, Color.Black);
+			LogColored(DecodeMessage(data), Color.White, rcv ? Color.DarkBlue : Color.DarkGreen);
+			logRtxt.AppendText("\n");
+
+			logRtxt.ScrollToCaret();
+		}
+		
+		string	GetEOL()
+		{
+			string	sel = eolCmb.Text;
+			if(sel == "LF")
+				return "\n";
+			else if(sel == "CR")
+				return "\r";
+			else if(sel == "CR LF")
+				return "\r\n";
+			else
+				return "";
+		}
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			try {
+				RegistryKey	key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\serialcomm");
+				portCmb.Text = (string)key.GetValue("COM_port");
+				baudCmb.Text = (string)key.GetValue("Baud_rate");
+				inputModeCmb.Text = (string)key.GetValue("Input_mode");
+				eolCmb.Text = (string)key.GetValue("EOL");
+			    lineBreakChk.Checked = (int) key.GetValue("LineBreak", 0) != 0;
+                autoConnectChk.Checked = (int)key.GetValue("AutoConnect", 0) != 0;
+			    logLimitCmb.Text = (string) key.GetValue("LogLimit");
+				
+				string list = (string)key.GetValue("CMDS");
+				usedCmds.AddRange(list.Split((char)255));
+				key.Close();
+			} catch {}
+			
+			inputTxt.AutoCompleteSource = AutoCompleteSource.CustomSource;
+			inputTxt.AutoCompleteCustomSource = usedCmds;
+
+		    logLimitCmb.Items.Add("No log limit");
+            foreach(var i in Enumerable.Range(1, 6))
+            {
+                logLimitCmb.Items.Add((1000*Math.Pow(2, i)) + " lines limit");
+            }
+		}
+
+        private void LimitLog()
+        {
+            int limit = logLimitCmb.SelectedIndex;
+            if(limit > 0)
+            {
+                limit *= 1000;
+                int excess = logRtxt.Lines.Length - limit;
+                if(excess > 0)
+                {
+                    logRtxt.Lines = logRtxt.Lines.Skip(excess).ToArray();
+                }
+            }
+        }
+
+	    void TextBox1KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode == Keys.Enter)
+			{
+				if(inputTxt.Text.Length == 0)
+					return;
+				
+				byte[]	msg = EncodeInputMessage(inputTxt.Text);
+				if(msg == null)
+				{
+					System.Windows.Forms.MessageBox.Show("Incorrect format");
+					return;
+				}
+				
+				try { ser.Write(msg, 0, msg.Length); } 
+				catch(Exception) { MessageBox.Show("Error writing to serial port");
+					return;
+				}
+				LogMsg(msg, false);
+				//richTextBox1.AppendText(DecodeMessage(msg) + '\n');
+				if(usedCmds.Count == 0 || usedCmds[usedCmds.Count-1] != inputTxt.Text)
+					usedCmds.Add(inputTxt.Text);
+				while(usedCmds.Count >= 1000)
+					usedCmds.RemoveAt(0);
+				
+				lastCmd = -1;
+				
+				inputTxt.Text = "";
+			}
+			else if(e.KeyCode == Keys.Up)
+			{
+				if(lastCmd == -1)
+					lastCmd = usedCmds.Count-1;
+				else if(lastCmd > 0)
+					lastCmd--;
+				
+				if(lastCmd >= 0 && lastCmd < usedCmds.Count)
+				{
+					inputTxt.Text = usedCmds[lastCmd];
+				}
+			}
+			else if(e.KeyCode == Keys.Down)
+			{
+				if(lastCmd == -1)
+					return;
+				else
+					lastCmd++;
+				
+				if(lastCmd >= usedCmds.Count)
+					lastCmd = usedCmds.Count - 1;
+				
+				if(lastCmd >= 0 && lastCmd < usedCmds.Count)
+				{
+					inputTxt.Text = usedCmds[lastCmd];
+				}
+			}
+		}
+		
+		void ComboBox2MouseClick(object sender, MouseEventArgs e)
+		{
+			portCmb.Items.Clear();
+			try {
+				portCmb.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+			} catch(Exception) {
+				
+			}
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(connectBtn.Text[0] == 'C')
+			{
+				if(portCmb.Text.Length == 0)
+					return;
+				
+				int baud = 0;
+				try { baud = Convert.ToInt32(baudCmb.Text); } catch {}
+				if(baud == 0)
+				{
+					MessageBox.Show("Choose baud rate");
+					return;
+				}
+
+                ser = new SerialPort();
+				
+				ser.BaudRate = baud;
+				ser.DataBits = 8;
+				ser.Handshake = Handshake.None;
+				ser.Parity = Parity.None;
+				ser.StopBits = StopBits.One;
+				ser.PortName = portCmb.Text;
+				ser.DtrEnable = false;
+				ser.RtsEnable = false;
+			    ser.DiscardNull = false;
+				
+				try { ser.Open(); }
+				catch(Exception ex) {
+					MessageBox.Show("Port open failed: " + ex.Message);
+                    CloseCom();
+                    return;
+				}
+				
+				connectBtn.Text = "Disconnect";
+				inputTxt.Enabled = true;
+				
+				rcvThread = new Thread((ThreadStart)ReceiveThread);
+				rcvThread.Start();
+			}
+			else
+			{
+				CloseCom();
+				connectBtn.Text = "Connect";
+				inputTxt.Enabled = false;
+			}
+		}
+		
+		void MainFormFormClosing(object sender, FormClosingEventArgs e)
+		{
+			CloseCom();
+			
+			try {
+				RegistryKey	key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\serialcomm");
+				key.SetValue("COM_port", portCmb.Text);
+				key.SetValue("Baud_rate", baudCmb.Text);
+				key.SetValue("Input_mode", inputModeCmb.Text);
+				key.SetValue("EOL", eolCmb.Text);
+				string	list = "";
+				for(int i = usedCmds.Count - 1, c = 0; i >= 0 && c < 100; i--, c++)
+				{
+					if(c != 0)
+						list += (char)255;
+					list += usedCmds[i];
+				}
+				key.SetValue("CMDS", list);
+                key.SetValue("LineBreak", lineBreakChk.Checked ? 1 : 0);
+                key.SetValue("AutoConnect", autoConnectChk.Checked ? 1 : 0);
+                key.SetValue("LogLimit", logLimitCmb.Text);
+				key.Close();
+			} catch {}
+		}
+		
+		void ComboBox4SelectedIndexChanged(object sender, EventArgs e)
+		{
+			char	mode = inputModeCmb.Text[0];
+			eolCmb.Enabled = mode == 'L';
+		}
+
+        private void MainFormActivated(object sender, EventArgs e)
+        {
+            if(ser == null && autoConnectChk.Checked)
+                Button1Click(null,null);
+        }
+
+        private void MainFormDeactivate(object sender, EventArgs e)
+        {
+            if (ser != null && autoConnectChk.Checked)
+                Button1Click(null, null);
+        }
+
+        private void Button2Click(object sender, EventArgs e)
+        {
+            logRtxt.Text = "";
+        }
+	}
+}
